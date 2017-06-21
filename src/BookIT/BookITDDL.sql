55993e64dd5cacce2806cb1f8299b430a2071c5f
Trenton Reeve <reevetb@dukes.jmu.edu>
2017-06-20 20:25:01 -0400

    uPDATED VERSION OF BOOKIT DDL

--- src/BookIT/BookITDDL.sql
+++ src/BookIT/BookITDDL.sql
@@ -18,3 +18,15 @@ CREATE TABLE EMPLOYEES
     PERMISSION_LEVEL    INTEGER,
     PRIMARY KEY         (EMP_ID)
 );
+
+CREATE TABLE PAYROLL
+(
+    DATE                DATE,
+    HOURS               INTEGER NOT NULL,
+    WAGE                NUMBER(7,2) NOT NULL,
+    OT_HOURS            INTEGER,
+    OT_WAGE             NUMBER(7,2),
+    EMP_ID              INTEGER,
+    PRIMARY KEY         (DATE),
+    FOREIGN KEY         (EMP_ID) REFERENCES EMPLOYEES
+);
