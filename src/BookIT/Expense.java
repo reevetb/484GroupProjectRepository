09c775c7c87ba98e07b5a7d3ed61f7d8d5ddf358
Daniel <Daniel@Daniels-Laptop>
2017-06-21 14:09:01 -0400

    expense privacy updated

--- src/BookIT/Expense.java
+++ src/BookIT/Expense.java
@@ -46,10 +46,7 @@ public class Expense {
     {
         this.expenseComments = comments;
     }
-    public void setID(int id)
-    {
-        this.expenseID = id;
-    }
+   
     
     public String getType()
     {
